03f0754e7deebd1f559742800f4337f4ddc396cd
Shudhoo <shudhowani@gmail.com>
2025-07-25 19:47:56 +0530

    25th July rebase conflicts solve 2

--- calculator.sh
+++ calculator.sh
@@ -2,8 +2,12 @@ echo "This is main branch changes"
 y=1-2-3-4
 x=1+2+3+4
 echo x
+<<<<<<< HEAD
 echo y 
 
 
 echo "This is test2 branch changes"
+
+echo "This is test2 branch changes 2nd commit"
+
 
